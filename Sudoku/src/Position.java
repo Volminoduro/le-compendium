17ffd79e61cd37ee0e02eea8793094db81f8ae4a
Volminoduro <steve.massicard@gmail.com>
2018-08-27 08:45:38 +0200

    Avancée Sudoku

---
+++ Sudoku/src/Position.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Position{
+
+    public int chiffreValide;
+
+    public List<Integer> chiffresPossibles = new ArrayList<Integer>();
+
+    public Position(){
+        chiffresPossibles = new ArrayList<Integer>();
+        for(int i = 1; i<Main.HAUTEUR_COTE *Main.LARGEUR_COTE; i++){
+            chiffresPossibles.add(i);
+        }
+    }
+
+    public Position(Integer chiffre){
+        setChiffreValide(chiffre);
+    }
+
+    public int getChiffreValide(){
+        return this.chiffreValide;
+    }
+
+    public void setChiffreValide(Integer chiffre){
+        this.chiffreValide=chiffre;
+        chiffresPossibles.clear();
+    }
+
+    public List<Integer> getChiffresPossibles(){
+        return this.chiffresPossibles;
+    }
+
+    public void setChiffresPossibles(List<Integer> chiffresPossibles){
+        this.chiffresPossibles = chiffresPossibles;
+    }
+
+    public String toString(){
+        return "Chiffre valide : "+this.chiffreValide;
+    }
+
+}
