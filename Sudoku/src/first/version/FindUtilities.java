e2f0d5e23f87d60b0223a58a911bb1549fad496c
x171230 <steve.massicart-ext@socgen.com>
2018-08-28 18:04:35 +0200

    [Sudoku] Résolu sur la 1ere version, perf amélio en 2nd version

--- Sudoku/src/first/version/FindUtilities.java
+++ Sudoku/src/first/version/FindUtilities.java
@@ -1,4 +1,4 @@
-package second.version;
+package first.version;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -6,108 +6,127 @@ import java.util.Map;
 public class FindUtilities {
 
     public static boolean trouverChiffreDansLaLigneVerticale(int positionVerticalDepart, int positionHorizontalDepart, int chiffreCherche){
-        int incrementRecherche = 0, longueurduCote;
+        boolean trouve = false;
+        int incrementRecherche = 0, incrementHorizontal = 0, incrementVertical = 0, pointDeDepartHorizontal = 0, pointDeDepartVertical = 0, longueurduCote, positionANePasInspecter;
 
+        incrementVertical++;
+        pointDeDepartHorizontal=positionHorizontalDepart;
+        positionANePasInspecter=positionVerticalDepart;
         longueurduCote = Main.HAUTEUR_COTE * Main.HAUTEUR_COTE;
 
-        while(incrementRecherche<longueurduCote){
-            if (incrementRecherche!=positionVerticalDepart ){
-                if(Main.plateau[0+(1*incrementRecherche)][positionHorizontalDepart]
+        while(incrementRecherche<longueurduCote && !trouve){
+            if (incrementRecherche!=positionANePasInspecter ){
+                if(Main.plateau[pointDeDepartVertical+(incrementVertical*incrementRecherche)][pointDeDepartHorizontal+(incrementHorizontal*incrementRecherche)]
                         .getChiffreValide() == chiffreCherche){
-                    return true;
+                    trouve = true;
                 }
             }
             incrementRecherche++;
         }
-        return false;
+        return trouve;
     }
 
     public static boolean trouverChiffrePossibleDansLaLigneVerticale(int positionVerticalDepart, int positionHorizontalDepart, int chiffreCherche){
-        int incrementRecherche = 0, longueurduCote;
+        boolean trouve = false;
+        int incrementRecherche = 0, incrementHorizontal = 0, incrementVertical = 0, pointDeDepartHorizontal = 0, pointDeDepartVertical = 0, longueurduCote, positionANePasInspecter;
 
+        incrementVertical++;
+        pointDeDepartHorizontal=positionHorizontalDepart;
+        positionANePasInspecter=positionVerticalDepart;
         longueurduCote = Main.HAUTEUR_COTE * Main.HAUTEUR_COTE;
 
-        while(incrementRecherche<longueurduCote){
-            if (incrementRecherche!=positionVerticalDepart ){
-                if(Main.plateau[0+(1*incrementRecherche)][positionHorizontalDepart]
+        while(incrementRecherche<longueurduCote && !trouve){
+            if (incrementRecherche!=positionANePasInspecter ){
+                if(Main.plateau[pointDeDepartVertical+(incrementVertical*incrementRecherche)][pointDeDepartHorizontal+(incrementHorizontal*incrementRecherche)]
                         .getChiffresPossibles().contains(chiffreCherche)){
-                    return true;
+                    trouve = true;
                 }
             }
             incrementRecherche++;
         }
-        return false;
+        return trouve;
     }
 
     public static boolean trouverChiffreDansLaLigneHorizontale(int positionVerticalDepart, int positionHorizontalDepart, int chiffreCherche){
-        int incrementRecherche = 0, longueurduCote;
+        boolean trouve = false;
+        int incrementRecherche = 0, incrementHorizontal = 0, incrementVertical = 0, pointDeDepartHorizontal = 0, pointDeDepartVertical = 0, longueurduCote, positionANePasInspecter;
 
+        incrementHorizontal++;
+        pointDeDepartVertical=positionVerticalDepart;
+        positionANePasInspecter=positionHorizontalDepart;
         longueurduCote = Main.LARGEUR_COTE * Main.LARGEUR_COTE;
 
-        while(incrementRecherche<longueurduCote){
-            if (incrementRecherche!=positionHorizontalDepart ){
-                if(Main.plateau[positionVerticalDepart][0+(1*incrementRecherche)]
+        while(incrementRecherche<longueurduCote && !trouve){
+            if (incrementRecherche!=positionANePasInspecter ){
+                if(Main.plateau[pointDeDepartVertical+(incrementVertical*incrementRecherche)][pointDeDepartHorizontal+(incrementHorizontal*incrementRecherche)]
                         .getChiffreValide() == chiffreCherche){
-                    return true;
+                    trouve = true;
                 }
             }
             incrementRecherche++;
         }
-        return false;
+        return trouve;
     }
 
     public static boolean trouverChiffrePossibleDansLaLigneHorizontale(int positionVerticalDepart, int positionHorizontalDepart, int chiffreCherche){
-        int incrementRecherche = 0, longueurduCote;
+        boolean trouve = false;
+        int incrementRecherche = 0, incrementHorizontal = 0, incrementVertical = 0, pointDeDepartHorizontal = 0, pointDeDepartVertical = 0, longueurduCote, positionANePasInspecter;
 
+        incrementHorizontal++;
+        pointDeDepartVertical=positionVerticalDepart;
+        positionANePasInspecter=positionHorizontalDepart;
         longueurduCote = Main.LARGEUR_COTE * Main.LARGEUR_COTE;
 
-        while(incrementRecherche<longueurduCote){
-            if (incrementRecherche!=positionHorizontalDepart ){
-                if(Main.plateau[positionVerticalDepart][0+(1*incrementRecherche)]
+        while(incrementRecherche<longueurduCote && !trouve){
+            if (incrementRecherche!=positionANePasInspecter ){
+                if(Main.plateau[pointDeDepartVertical+(incrementVertical*incrementRecherche)][pointDeDepartHorizontal+(incrementHorizontal*incrementRecherche)]
                         .getChiffresPossibles().contains(chiffreCherche)){
-                    return true;
+                    trouve = true;
                 }
             }
             incrementRecherche++;
         }
-        return false;
+        return trouve;
     }
 
     public static boolean trouverChiffreDansLaZone(int positionVerticalDepart, int positionHorizontalDepart, int ChiffreCherche){
+        boolean trouve = false;
         int compteurVertical = 0, compteurHorizontal;
         // Cela me donne le début de la zone
         Map<String, Integer> debutDeZone = getDebutDeZone(positionVerticalDepart, positionHorizontalDepart);
 
-        while(compteurVertical< Main.HAUTEUR_COTE){
+        while(!trouve && compteurVertical< Main.HAUTEUR_COTE){
             compteurHorizontal = 0;
-            while(compteurHorizontal< Main.LARGEUR_COTE){
+            while(!trouve && compteurHorizontal< Main.LARGEUR_COTE){
                 if(Main.plateau[debutDeZone.get("HAUTEUR")+compteurVertical][debutDeZone.get("LARGEUR")+compteurHorizontal].getChiffreValide() == ChiffreCherche){
-                    return true;
+                    trouve=true;
                 }
                 compteurHorizontal++;
             }
             compteurVertical++;
         }
 
-        return false;
+        return trouve;
     }
 
     public static boolean trouverChiffrePossibleDansLaZone(int positionVerticalDepart, int positionHorizontalDepart, int chiffreCherche){
+        boolean trouve = false;
         int compteurVertical = 0, compteurHorizontal = 0;
         // Cela me donne le début de la zone
         Map<String, Integer> debutDeZone = getDebutDeZone(positionVerticalDepart, positionHorizontalDepart);
 
-        while(compteurVertical< Main.HAUTEUR_COTE){
+        while(!trouve && compteurVertical< Main.HAUTEUR_COTE){
             compteurHorizontal=0;
-            while(compteurHorizontal< Main.LARGEUR_COTE){
+            while(!trouve && compteurHorizontal< Main.LARGEUR_COTE){
                 if(Main.plateau[debutDeZone.get("HAUTEUR")+compteurVertical][debutDeZone.get("LARGEUR")+compteurHorizontal].getChiffresPossibles().contains(chiffreCherche)){
-                    return true;
+                    trouve=true;
                 }
                 compteurHorizontal++;
             }
             compteurVertical++;
         }
-        return false;
+
+        return trouve;
     }
 
     public static Map<String, Integer> getDebutDeZone (int positionVerticalDepart, int positionHorizontalDepart){
